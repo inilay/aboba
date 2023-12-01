6bc76fb078d1bb127d98ba0a4670a63e60b0402c
inilay <110691997+inilay@users.noreply.github.com>
2023-12-01 18:35:28 +0300

    Create ram.py

---
+++ deepra_test/ram.py
@@ -0,0 +1,14 @@
+import argparse
+import time
+ 
+def generate_ram_load(interval=int(sys.argv[1]),amount=int(sys.argv[2])):
+    GB = 1024 * 1024 * 1024
+    eat = "a" * GB * amount
+    time.sleep(interval)
+ 
+#----START OF SCRIPT
+if __name__=='__main__':
+    print("Load RAM")
+    if len(sys.argv)>2:
+       generate_ram_load()
+       

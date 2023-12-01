abb50c0772cbdffaf8d2a622f201cddb785b7ea6
inilay <110691997+inilay@users.noreply.github.com>
2023-12-01 18:52:30 +0300

    Update ram.py

--- deepra_test/ram.py
+++ deepra_test/ram.py
@@ -1,11 +1,16 @@
 import argparse
 import time
 import sys
+
  
 def generate_ram_load(interval=int(sys.argv[1]),amount=int(sys.argv[2])):
     GB = 1024 * 1024 * 1024
-    eat = "a" * GB * amount
-    time.sleep(interval)
+    count = 0
+    while(count < interval):
+    	eat = "a" * GB * amount
+    	time.sleep(1)
+    	count += 1
+    	print('Iter', count)
  
 #----START OF SCRIPT
 if __name__=='__main__':
